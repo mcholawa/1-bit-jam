e1c9666e6a13b482205ba37cc08f2575aabb6122
mcholawa <69717419+mcholawa@users.noreply.github.com>
2023-08-12 17:17:43 +0200

    Feat GameMenager posts array and finding all posts

--- 1-bit-jam/Assets/Scripts/GameMenager.cs
+++ 1-bit-jam/Assets/Scripts/GameMenager.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class GameMenager : MonoBehaviour
 {
+    public GameObject[] posts;
     // Start is called before the first frame update
     void Start()
     {
-        
+        posts = GameObject.FindGameObjectsWithTag("Post").OrderBy(go => go.transform.position.x).ToArray();
+        Debug.Log(posts[0]);
     }
 
     // Update is called once per frame
