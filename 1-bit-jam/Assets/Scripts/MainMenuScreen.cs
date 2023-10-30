172d8d07476a10ce4bd47303fad8c7ef5089d19c
mcholawa <69717419+mcholawa@users.noreply.github.com>
2023-10-30 15:25:21 +0100

    Added basic MainMenu
    And simple button naviagtion (starting the game, and going to the main menu from GameOver Screen and winning screen

---
+++ 1-bit-jam/Assets/Scripts/MainMenuScreen.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start(){
+        gameObject.SetActive(true);
+
+    }
+    public void StartButton(){
+        SceneManager.LoadScene("SampleScene");
+    }
+}
