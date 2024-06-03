60b928c6b7619c174974bc5f7a471b19456db7ff
mcholawa <69717419+mcholawa@users.noreply.github.com>
2024-06-03 16:52:14 +0200

    Reworked Horizontal movement

--- 1-bit-jam/Assets/Scripts/PlayerController.cs
+++ 1-bit-jam/Assets/Scripts/PlayerController.cs
@@ -18,6 +18,10 @@ public class PlayerController : MonoBehaviour
     private bool isExpanding = false;
     private Vector3 scaleChange = new Vector3(1f, 1f, 1f);
     public float scaleSpeed = 4;
+    public bool IsAvailable = true;
+    public float CooldownDuration;
+    private GameObject[] currentPosts;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +30,7 @@ public class PlayerController : MonoBehaviour
         gameMenagerScript = gameMenagerObject.GetComponent<GameMenager>();
         //Debug.Log(gameMenagerScript.posts.Length);
         spriteMask = gameObject.transform.GetChild(0).gameObject;
+        currentPosts = gameMenagerScript.posts;
     }
 
     // Update is called once per frame
@@ -34,20 +39,24 @@ public class PlayerController : MonoBehaviour
         PlayerMovement();
         RotateSpriteMaks();
         //scaling the mask 
-        if(isExpanding){
+        if (isExpanding)
+        {
             spriteMask.transform.localScale += scaleChange * Time.deltaTime * scaleSpeed;
-            if(Vector3.Distance(spriteMask.transform.localScale, new Vector3(16f, 16f, 16f)) < 1.0f ){
+            if (Vector3.Distance(spriteMask.transform.localScale, new Vector3(16f, 16f, 16f)) < 1.0f)
+            {
                 scaleChange = -scaleChange;
             }
-            else if(Vector3.Distance(spriteMask.transform.localScale, new Vector3(4f, 4f, 4f)) < 1.0f && scaleChange.x < 0){
-                    isExpanding = false;
-                    scaleChange = -scaleChange;
+            else if (Vector3.Distance(spriteMask.transform.localScale, new Vector3(4f, 4f, 4f)) < 1.0f && scaleChange.x < 0)
+            {
+                isExpanding = false;
+                scaleChange = -scaleChange;
             }
         }
     }
-    void RotateSpriteMaks(){
+    void RotateSpriteMaks()
+    {
         float degreesPerSecond = 50;
-        spriteMask.transform.Rotate( new Vector3(0, 0,degreesPerSecond ) * Time.deltaTime);
+        spriteMask.transform.Rotate(new Vector3(0, 0, degreesPerSecond) * Time.deltaTime);
     }
     //collision manager
     void OnCollisionEnter2D(Collision2D col)
@@ -61,119 +70,27 @@ public class PlayerController : MonoBehaviour
         {
             gameMenagerScript.ChangeEnergyAmount(15);
             col.gameObject.SetActive(false);
-            
+
         }
-        else if (col.gameObject.name== "Wall"){
+        else if (col.gameObject.name == "Wall")
+        {
             gameMenagerScript.GameOver();
         }
 
     }
     //Function used in Array.Find for checking UpArrow and DownArrow movement :)
-    private bool findMatchingPostX(GameObject upPost){
+    private bool findMatchingPostX(GameObject upPost)
+    {
         float playerPositionX = transform.position.x;
-            return upPost.transform.position.x == playerPositionX;
-        }
+        return upPost.transform.position.x == playerPositionX;
+    }
     void PlayerMovement()
     {
-        float distanceToDestination = 0;
-        int currentPostsLength;
-        Transform nextTarget = null;
-        //Key Input 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            Debug.Log("right");
-            if(isUp){
-            currentPostsLength = gameMenagerScript.upPosts.Length;
-            distanceToDestination = Vector3.Distance(transform.position, gameMenagerScript.upPosts[movementIndex].transform.GetChild(0).position);
-                if(movementIndex < currentPostsLength - 1) {nextTarget = gameMenagerScript.upPosts[movementIndex + 1].transform.GetChild(0);}
-            }else{
-            currentPostsLength = gameMenagerScript.posts.Length;
-            distanceToDestination = Vector3.Distance(transform.position, gameMenagerScript.posts[movementIndex].transform.GetChild(0).position);
-                if(movementIndex < currentPostsLength - 1) {nextTarget = gameMenagerScript.posts[movementIndex +1].transform.GetChild(0);}
-                
-            }
-            if (movementIndex < currentPostsLength - 1 && distanceToDestination < 1.5 && distanceToDestination <= 6)
-            {
-                float distanceToNext = Vector3.Distance(transform.position, nextTarget.position);
-                    movementIndex++;
-                    Debug.Log("Moving");
-                    isMoving = true;
-                    gameMenagerScript.ChangeEnergyAmount(energyCost);
-                
-            }
-            //if you are on the last platform
-            else{
-                Debug.Log("Bzzzz");
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            Debug.Log("left");
-            if(isUp && movementIndex > 0){
-            distanceToDestination = Vector3.Distance(transform.position, gameMenagerScript.upPosts[movementIndex].transform.GetChild(0).position);
-            nextTarget = gameMenagerScript.upPosts[movementIndex-1].transform.GetChild(0);
-            }else if(!isUp && movementIndex > 0){
-            distanceToDestination = Vector3.Distance(transform.position, gameMenagerScript.posts[movementIndex].transform.GetChild(0).position);
-            nextTarget = gameMenagerScript.upPosts[movementIndex-1].transform.GetChild(0);
-            }
-            //If you are on the first platform:
-            else if(movementIndex == 0){
-                Debug.Log("Bz");
-            }
 
-            if (movementIndex > 0 && distanceToDestination < 1.5 && distanceToDestination <= 6)
-            {
-                float distanceToNext = Vector3.Distance(transform.position, nextTarget.position);
-                Debug.Log(distanceToNext);
-                    movementIndex--;
-                    Debug.Log("moving");
-                    isMoving = true;
-                    gameMenagerScript.ChangeEnergyAmount(energyCost);
-    
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.UpArrow) && !isUp){
-            Debug.Log("up");
-            GameObject matchingPostFound = Array.Find(gameMenagerScript.upPosts, findMatchingPostX);
-             if(matchingPostFound != null){
-                Debug.Log(matchingPostFound.name); 
-                movementIndex = Array.IndexOf(gameMenagerScript.upPosts, matchingPostFound);
-                Debug.Log("moving");
-                isUp = true;
-                isMoving = true;
-                gameMenagerScript.ChangeEnergyAmount(energyCost);
-             }
-             else{
-              Debug.Log("Nie ma tam nic :(");
-             }
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow) && isUp){
-            Debug.Log("down");
-             GameObject matchingPostFound = Array.Find(gameMenagerScript.posts, findMatchingPostX);
-             if(matchingPostFound != null){
-                Debug.Log(matchingPostFound.name); 
-                movementIndex = Array.IndexOf(gameMenagerScript.posts, matchingPostFound);
-                Debug.Log("moving");
-                isUp = false;
-                isMoving = true;
-                gameMenagerScript.ChangeEnergyAmount(energyCost);
-             }
-             else{
-               Debug.Log("Nie ma tam nic :(");
-             }
-        }
-        else if(Input.GetKeyDown(KeyCode.Space)){
-            Debug.Log("Space");
-            spaceEvent();
-        }
         if (isMoving)
         {
             Transform newPosition;
-            if(isUp){
-            newPosition = gameMenagerScript.upPosts[movementIndex].transform.GetChild(0);
-            }else{
-            newPosition = gameMenagerScript.posts[movementIndex].transform.GetChild(0);
-            }
+            newPosition = currentPosts[movementIndex].transform.GetChild(0);
             if (transform.position == newPosition.position)
             {
                 isMoving = false;
@@ -183,11 +100,84 @@ public class PlayerController : MonoBehaviour
                 transform.position = Vector3.MoveTowards(transform.position, newPosition.position, speed * Time.deltaTime);
             }
         }
+        if (IsAvailable)
+        {
+            //if right arrow is pressed AND it is not the last post AND there is no gap
+            if (Input.GetKeyDown(KeyCode.RightArrow) && movementIndex < currentPosts.Length - 1 && 6 >= Vector2.Distance(currentPosts[movementIndex].transform.position,currentPosts[movementIndex+1].transform.position))
+            {
+                Debug.Log("right");
+                movementIndex++;
+                isMoving = true;
+                StartCoroutine(StartCooldown());
+                gameMenagerScript.ChangeEnergyAmount(energyCost);
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow) && movementIndex != 0  && 6 >= Vector2.Distance(currentPosts[movementIndex].transform.position,currentPosts[movementIndex-1].transform.position))
+            {
+                Debug.Log("left");
+                movementIndex--;
+                isMoving = true;
+                StartCoroutine(StartCooldown());
+                gameMenagerScript.ChangeEnergyAmount(energyCost);
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow) && !isUp)
+            {
+                GameObject matchingPostFound = Array.Find(gameMenagerScript.upPosts, findMatchingPostX);
+                 if(matchingPostFound != null){
+                    movementIndex = Array.IndexOf(gameMenagerScript.upPosts, matchingPostFound);
+                    currentPosts = gameMenagerScript.upPosts;
+                    isMoving = true;
+                    isUp = true;
+                    Debug.Log("up");
+                    StartCoroutine(StartCooldown());
+                }
+               
+
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow) && isUp)
+            {
+                GameObject matchingPostFound = Array.Find(gameMenagerScript.posts, findMatchingPostX);
+                if(matchingPostFound != null){
+                movementIndex = Array.IndexOf(gameMenagerScript.posts, matchingPostFound);
+                isMoving = true;
+                 currentPosts = gameMenagerScript.posts;
+                isUp = false;
+                Debug.Log("down");
+                StartCoroutine(StartCooldown());
+                }
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space");
+            //print all posts
+            foreach (var item in currentPosts)
+            {
+                Debug.Log(item.ToString());
+            }
+            Debug.Log("movement index:");
+            Debug.Log(movementIndex);
+            spaceEvent();
+        }
     }
-    void spaceEvent(){
-        if (gameMenagerScript.energy > 10 && !isExpanding){
+    //Key Input 
+    void spaceEvent()
+    {
+        if (gameMenagerScript.energy > 10 && !isExpanding)
+        {
             isExpanding = true;
             gameMenagerScript.ChangeEnergyAmount(-10);
         }
+    }
+
+    public IEnumerator StartCooldown()
+    {
+        IsAvailable = false;
+        Debug.Log(CooldownDuration);
+        playerSprite.color = new Color (1f,1f,1f,0.3f);
+        yield return new WaitForSeconds(CooldownDuration);
+
+        IsAvailable = true;
+        Debug.Log("not Cooldown");
+        playerSprite.color = new Color (1f,1f,1f,1f);
     }
 }
