5fc26584af2797c6b77d979c6deb941604ce0698
mcholawa <69717419+mcholawa@users.noreply.github.com>
2024-07-04 14:33:23 +0200

    Added TypewriterEffect to NarrationText

--- 1-bit-jam/Assets/Scripts/TypewriterEffect.cs
+++ 1-bit-jam/Assets/Scripts/TypewriterEffect.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.Collections;
 
 public class TypewriterEffect : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float delay = 0.3f;
+    public string fullText = "rabarbar";  // Set your desired text here
+    private string currentText = "";
+    private TMP_Text textComponent;
+
     void Start()
     {
-        
+        textComponent = GetComponent<TMP_Text>();
+        Debug.Log("Start method executed. Text component found: " + (textComponent != null));
+        Debug.Log("Full text: " + fullText);
+        StartCoroutine(ShowText());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ShowText()
     {
-        
+        for (int i = 0; i <= fullText.Length; i++)
+        {
+            currentText = fullText.Substring(0, i);
+            textComponent.text = currentText;
+            Debug.Log("Current text: " + currentText);
+            yield return new WaitForSeconds(delay);
+        }
     }
 }
